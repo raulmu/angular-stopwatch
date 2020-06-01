e781643563d67834587ac0984830e10633c13743
raulmu <raulmu@outlook.com>
2020-06-01 13:35:44 -0300

    1 file modified, 3 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,61 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
+export class AppComponent {
   seconds = 10;
   isCounting = false;
   interval;
-  name = 'Angular ' + VERSION.major;
-  
+  name = "Angular " + VERSION.major;
+
   start() {
-    if(!this.isCounting){
-      this.isCounting=true;
-      console.log('has started');
-      // let audio = new Audio();
-      // audio.src = '../assets/sounds/beep-start.ogg';
-      // source.setAttribute('src', 'AUDIO_URL');
+    if (!this.isCounting) {
+      this.isCounting = true;
+      console.log("started");
       const audio = new Audio();
-      const source = document.createElement('source');
-
-      source.setAttribute('src', '../assets/sounds/1.mp3');
-      source.setAttribute('type', 'audio/mpeg');
-
+      const source = document.createElement("source");
+      source.setAttribute(
+        "src",
+        "https://raw.githubusercontent.com/raulmu/angular-stopwatch/master/src/assets/sounds/beep-start.mp3"
+      );
+      source.setAttribute("type", "audio/mpeg");
       audio.append(source);
+      audio.load();
       audio.play();
-
-
-      // audio.setAttribute('type', 'audio/ogg');
-      // audio.load();
-      // audio.play();
-      this.interval = window.setInterval(
-        () => this.toDo(), 1000
-        );
+      window.setTimeout(() => {
+        this.interval = window.setInterval(() => this.toDo(), 1000);
+      }, 3500);
     }
   }
 
-  toDo(){
-    if(!this.seconds) {
+  toDo() {
+    if (!this.seconds) {      const audio = new Audio();
+      const source = document.createElement("source");
+      source.setAttribute(
+        "src",
+        "https://raw.githubusercontent.com/raulmu/angular-stopwatch/master/src/assets/sounds/trim-stop.mp3"
+      );
+      source.setAttribute("type", "audio/mpeg");
+      audio.append(source);
+      audio.load();
+      audio.play();
       window.clearInterval(this.interval);
       this.seconds = 10;
       this.isCounting = false;
+      console.log("finished");
     } else {
       console.log(this.seconds);
-      this.seconds=this.seconds - 1;
+      this.seconds = this.seconds - 1;
     }
   }
 
-
+  stop() {
+    this.isCounting = false;
+    window.clearInterval(this.interval);
+    this.seconds = 10;
+    console.log("stoped");
+  }
 }
